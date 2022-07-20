609dcabe015201058eaefe30973ca4c1779ff8e9
Robert Gitei <robert.gitei@yopeso.com>
2022-07-20 15:03:22 +0300

    Make text more readable

--- app/src/main/java/com/example/cryptoapp/CryptoCoin.kt
+++ app/src/main/java/com/example/cryptoapp/CryptoCoin.kt
@@ -4,8 +4,15 @@ data class CryptoCoin(
     var id: String,
     var name: String,
     var symbol: String,
-    var rank : Int,
+    var rank: Int,
     var is_new: Boolean,
     var is_active: Boolean,
     var type: String
-)
+) {
+    override fun toString(): String {
+        return "#$rank $symbol ($name), " +
+                if (is_new) "NEW, " else "OLD, " +
+                if (is_active) "ACTIVE, " else "INACTIVE, " +
+                "$type;"
+    }
+}
