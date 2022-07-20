b86da42b3ed30cb0b9867a82328c7dfdac35d226
Robert Gitei <robert.gitei@yopeso.com>
2022-07-20 14:36:03 +0300

    Read the json and convert to objects

--- app/src/main/java/com/example/cryptoapp/MainActivity.kt
+++ app/src/main/java/com/example/cryptoapp/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.cryptoapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.example.cryptoapp.databinding.ActivityMainBinding
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
+import java.io.IOException
+
+const val TAG = "MainActivity"
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,6 +20,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val cryptoList = getCryptoCoins(this)
+        for(i in cryptoList)
+            Log.i(TAG, i.toString())
+        Log.i(TAG, cryptoList[0].javaClass.toString())
 
+    }
+
+    fun getCryptoCoins(context: Context): List<CryptoCoin> {
+        lateinit var jsonString: String
+        try {
+            jsonString = context.resources.openRawResource(R.raw.input)
+                .bufferedReader()
+                .use { it.readText() }
+        } catch (ioException: IOException) {
+            Log.e(TAG, "oops n-am reusit sa citim bine")
+            return emptyList()
+        }
+
+        val cryptoList = object : TypeToken<List<CryptoCoin>>() {}.type
+        return Gson().fromJson(jsonString, cryptoList)
     }
 }
