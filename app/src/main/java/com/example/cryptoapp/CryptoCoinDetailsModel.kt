178d94c33c71aa3e41401a2a2e364dfbf8dea354
Robert Gitei <robert.gitei@yopeso.com>
2022-07-21 12:49:19 +0300

    Create CryptoCoinDetailsModel data class

---
+++ app/src/main/java/com/example/cryptoapp/CryptoCoinDetailsModel.kt
@@ -0,0 +1,42 @@
+package com.example.cryptoapp
+
+import com.google.gson.annotations.SerializedName
+
+data class CryptoCoinDetailsModel(
+    val id: String = "",
+    val name: String = "",
+    val symbol: String = "",
+    val rank: Int = 0,
+    @SerializedName("is_new")
+    val isNew: Boolean = false,
+    @SerializedName("is_active")
+    val isActive: Boolean = false,
+    val type: String = "",
+    val contract: String = "",
+    val platform: String = "",
+    val contracts: List<ContractModel> = emptyList(),
+    val parent: ParentModel = ParentModel(),
+    val tags: List<TagModel> = emptyList(),
+    val team: List<TeamModel> = emptyList(),
+    val description: String = "",
+    val message: String = "",
+    @SerializedName("open_source")
+    val openSource: Boolean = false,
+    @SerializedName("started_at")
+    val startedAt: String = "",
+    @SerializedName("development_status")
+    val developmentStatus: String = "",
+    @SerializedName("hardware_wallet")
+    val hardwareWallet: Boolean = false,
+    @SerializedName("proof_type")
+    val proofType: String = "",
+    @SerializedName("org_structure")
+    val orgStructure: String = "",
+    @SerializedName("hash_algorithm")
+    val hashAlgorithm: String = "",
+    val links: LinksModel = LinksModel(),
+    @SerializedName("links_extended")
+    val linksExtended: List<LinkExtendedModel> = emptyList(),
+    val whitepaper: WhitepaperModel = WhitepaperModel()
+) {
+}
