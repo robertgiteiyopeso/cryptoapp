d6a7b96f89d397430c60b93badf2165cea1be8cc
Robert Gitei <robert.gitei@yopeso.com>
2022-08-02 11:56:49 +0300

    Create login activity and fragments

---
+++ app/src/main/java/com/example/cryptoapp/LoginActivity.kt
@@ -0,0 +1,21 @@
+package com.example.cryptoapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.cryptoapp.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportFragmentManager.beginTransaction()
+            .add(R.id.fragment_container_view_tag, LoginFragment()).commit()
+
+    }
+}
